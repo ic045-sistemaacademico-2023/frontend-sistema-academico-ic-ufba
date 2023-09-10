4c9983d8dbff3b0b191f4b82c208656237f65743
Lávio <laviovale@gmail.com>
2023-09-10 09:24:06 -0300

    added axios

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const baseURL = "localhost:3001";
+
+const headers = {
+  "Content-Type": "application/json",
+  Accept: "application/json",
+}
+
+const api = axios.create({
+  baseURL,
+  headers,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  return config;
+});
+
+export default api;
