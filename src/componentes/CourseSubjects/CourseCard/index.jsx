9c567ea2c3b6bc965d52e2c8d51170f4b3e80932
Lávio <laviovale@gmail.com>
2023-09-15 21:08:36 -0300

    added green color for done subjects

--- src/componentes/CourseSubjects/CourseCard/index.jsx
+++ src/componentes/CourseSubjects/CourseCard/index.jsx
@@ -1,8 +1,12 @@
 export default function CourseCard({ data }) {
   return (
-    <div className="flex flex-col text-center justify-center bg-primary-200 rounded w-52 h-28 hover:bg-primary-400 cursor-pointer">
+    <div
+      className={`flex flex-col text-center justify-center bg-primary-200 rounded w-52 h-28 hover:bg-primary-400 cursor-pointer ${
+        data?.concluido ? "bg-green-100 hover:bg-green-200" : ""
+      }`}
+    >
       <p>{data.codigo}</p>
       <p>{data.nome}</p>
     </div>
-  )
+  );
 }
