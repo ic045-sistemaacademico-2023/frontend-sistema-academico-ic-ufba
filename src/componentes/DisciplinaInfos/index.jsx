bc06b2f52e4c431cc767a13bfdc8052214c0ae7e
Glauber Gouveia <74075357+GlauberGouveia@users.noreply.github.com>
2023-09-17 12:53:48 -0300

    Add files via upload

--- src/componentes/DisciplinaInfos/index.jsx
+++ src/componentes/DisciplinaInfos/index.jsx
@@ -1,11 +1,11 @@
 function DisciplinaInfos({ course }) {
     return (
-        <div className="bg-primary-50 p-5 z-10 m-5 shadow-lg rounded-lg">
-            <h1 className="text-2xl text-primary-700 font-bold mb-6 border-b pb-2">
+        <div className="bg-primary-50 p-1 z-10 m-1 rounded-lg">
+            <h1 className="text-2xl text-primary-700 font-bold mb-6 pb-2">
                 Ementa da Disciplina
             </h1>
 
-            <div className="grid grid-cols-3 gap-6 mb-6">
+            <div className="grid grid-cols-3 gap-6 mb-6 bg-primary-100 border-2 py-3">
                 <div className>
                     <strong className="text-sm text-primary-600 block">Disciplina:</strong>
                     <p className="text-lg font-medium text-primary-800">{course.componenteCurricular}</p>
@@ -18,34 +18,44 @@ function DisciplinaInfos({ course }) {
                     <strong className="text-sm text-primary-600 block">Departamento:</strong>
                     <p className="text-lg font-medium text-primary-800">{course.departamento}</p>
                 </div>
+                <div>
+                    <strong className="text-sm text-primary-600 block">Carga Horária Total(CH):</strong>
+                    <p className="text-lg font-medium text-primary-800">{course.ch} hrs</p>
+                </div>
                 <div >
                     <strong className="text-sm text-primary-600 block">Carga Horária Prática:</strong>
                     <p className="text-lg font-medium text-primary-800">{course.chPratica} hrs</p>
-                </div>
-                <div>
-                    <strong className="text-sm text-primary-600 block">Carga Horária (CH):</strong>
-                    <p className="text-lg font-medium text-primary-800">{course.ch} hrs</p>
-                </div>
+                </div>                
                 <div>
                     <strong className="text-sm text-primary-600 block">Carga Horária Teórica:</strong>
                     <p className="text-lg font-medium text-primary-800">{course.chTeorica} hrs</p>
                 </div>
             </div>
 
-            <div className="mb-6">
-                <strong className="text-sm text-primary-600">Ementa:</strong>
-                <p className="text-lg text-primary-700 mt-2">{course.ementa}</p>
+            <div className="mb-6 pt-4 border-t-2 text-left">
+                <strong className="text-xl text-primary-600" >Ementa:</strong>
+                <p className="text-base text-primary-700 mt-2 text-justify">{course.ementa}</p>
             </div>
 
-            <div className="mb-6">
-                <strong className="text-sm text-primary-600">Bibliografia:</strong>
-                <p className="text-lg text-primary-700 mt-2">{course.bibliografia.join(", ")}</p>
+            <div className="mb-6 text-left">
+                <strong className="text-xl text-primary-600">Objetivos:</strong>
+                    <p className="text-base text-primary-700 mt-2 text-justify">{course.objetivos}</p>
             </div>
 
-            <div className="mb-6">
-                <strong className="text-sm text-primary-600">Conteúdo:</strong>
-                <p className="text-lg text-primary-700 mt-2">{course.conteudo}</p>
+            <div className="mb-6 text-left">
+                <strong className="text-xl text-primary-600">Conteúdo:</strong>
+                {course.conteudo.split('\n').map((item, i) => (
+                    <p className="text-base text-primary-700 mt-2 text-justify" key={i}>{item}</p>
+                ))}
             </div>
+
+            <div className="mb-2 text-left border-b-2 pb-2">
+                <strong className="text-xl text-primary-600">Bibliografia:</strong>
+                {course.bibliografia.split('\n').map((item, i) => (
+                    <p className="text-base text-primary-700 mt-2 text-justify" key={i}>{item}</p>
+                ))}
+            </div>
+
         </div>
     );
 }
