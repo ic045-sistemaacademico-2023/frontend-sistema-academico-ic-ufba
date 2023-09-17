9a798d3b9fe9141a112784cf3c9021f4aa555df3
Glauber Gouveia <74075357+GlauberGouveia@users.noreply.github.com>
2023-09-17 12:50:07 -0300

    Add files via upload

--- src/pages/EmentaDisciplina/data.js
+++ src/pages/EmentaDisciplina/data.js
@@ -4,8 +4,9 @@ export const courseData = {
     ch: 60,
     chPratica: 40,
     chTeorica: 20,
-    departamento: "Teoria Econômica",
+    departamento: "Economia",
     ementa: "Os sistemas econômicos: formação e evolução. O liberalismo econômico e a fase mercantilista. A combinação dos fatores de produção. Categorias econômicas: renda, lucro, juros e salário. A teoria do valor. Concorrência, formação de preços e mercado. Globalização e a ordem econômica internacional. Noções sobre os principais conceitos da teoria econômica em seus vieses microeconômico e macroeconômico, além de temas específicos como: moeda, Estado e relações externas.",
-    bibliografia: ["Não há Bibliografia cadastrada",],
-    conteudo: "Não há Conteúdo cadastrado",
+    objetivos: "Introduzir os conceitos fundamentais da economia. Compreender os diferentes sistemas econômicos. Analisar a formação de preços e o mercado. Discutir a globalização e a ordem econômica internacional.",
+    conteudo: "1. Introdução à Economia\n2. Sistemas Econômicos\n3. Liberalismo Econômico\n4. Fase Mercantilista\n5. Fatores de Produção\n6. Categorias Econômicas\n7. Teoria do Valor\n8. Concorrência e Formação de Preços",
+    bibliografia: "1. MANKIW, N.Gregory; TAYLOR, Mark P.. Princípios de economia. 6.ed., São Paulo: Cengage Learning, 2014.\n2. VASCONCELLOS, Marco Antonio Sandoval de; GARCIA, Manuel E.. Fundamentos de economia. 5.ed., São Paulo: Saraiva, 2014.\n3. KRUGMAN, Paul; WELLS, Robin; OLNEY, Martha L.. Introdução à economia. 2.ed., Rio de Janeiro: Elsevier, 2010.",
 };
--- src/pages/EmentaDisciplina/index.jsx
+++ src/pages/EmentaDisciplina/index.jsx
@@ -1,14 +1,18 @@
-import DisciplinaEmenta from "../../componentes/DisciplinaInfos";
 import Sidebar from "../../componentes/Sidebar";
 
 import { courseData } from "./data";
+import { courseClasses } from "./courses";
 import DisciplinaInfos from "../../componentes/DisciplinaInfos";
+import CourseClasses from "../../componentes/CourseClasses";
 
 function EmentaDisciplinaPage() {
     return (
         <div className="w-full pl-64">
             <Sidebar />
+            <div className="bg-primary-50 p-1 z-10 m-4 shadow-lg rounded-lg max-w-5xl border-2">
             <DisciplinaInfos course={courseData} />
+            <CourseClasses courseClasses={courseClasses} />
+            </div>
         </div>
     );
 }
