d512e881b068bdbf57665b6f9f2764d95f9c0c28
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2023-09-29 09:28:56 -0300

    GET Users integracao

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,8 +1,18 @@
-import { users } from "./data.js";
 import UserTable from "../../componentes/UsersTable/index.jsx";
 import Sidebar from "../../componentes/Sidebar/index.jsx";
 
+import api from "../../utils/api.js";
+import { useEffect, useState } from "react";
+
 function UsersPage() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    api.get("/user/all").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
   return (
     <div className="w-full pl-64 pt-10">
       <Sidebar />
