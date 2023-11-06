349cb422fe84adf04cb005d382ea666e85f77838
Lucas Natanael <lucas.natan.prates@hotmail.com>
2023-11-06 18:32:36 -0300

    Update src/pages/History/index.jsx
    Co-authored-by: Lávio Vale <44332001+laviodias@users.noreply.github.com>

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -63,7 +63,7 @@ function HistoryPage() {
   }, []);
 
   return (
-    studentData != null && (
+    studentData && (
       <div className="w-full pl-64">
         <Sidebar />
         <div className="printable">
