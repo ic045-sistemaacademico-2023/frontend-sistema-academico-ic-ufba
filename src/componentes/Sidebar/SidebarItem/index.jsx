0103cc4b25468503534e6c222dfed09015a70ac5
Lávio <laviovale@gmail.com>
2023-11-18 13:06:03 -0300

    fix link

--- src/componentes/Sidebar/SidebarItem/index.jsx
+++ src/componentes/Sidebar/SidebarItem/index.jsx
@@ -1,19 +1,19 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function SidebarItem({ icon, title, link }) {
   const location = useLocation();
   const isActive = location.pathname === link;
   return (
     <li>
-      <a
-        href={link}
+      <Link
+        to={link}
         className={`flex items-center p-2 rounded-lg hover:text-gray-800 hover:bg-primary-100 group ${
           isActive ? "bg-primary-100 text-gray-800" : "text-white"
         }`}
       >
         {icon}
         <span className="ml-3 max-w-[182px] text-left">{title}</span>
-      </a>
+      </Link>
     </li>
   );
 }
