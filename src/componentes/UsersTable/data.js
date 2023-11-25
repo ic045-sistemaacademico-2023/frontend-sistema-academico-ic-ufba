0bab99969869f1fd0e93f6b290b1d5b9cab5c44e
glaubergouveia <glauber.s.gouveia@gmail.com>
2023-11-25 18:36:46 -0300

    Paginação da UsersTable

--- src/componentes/UsersTable/data.js
+++ src/componentes/UsersTable/data.js
@@ -23,3 +23,11 @@ export const roleFilterOptions = [
   { id: "professorFilterId", value: "PROFESSOR", name: "Professor" },
   { id: "alunoFilterId", value: "ALUNO", name: "Aluno" },
 ];
+
+export const roleShowByPageOptions = [
+  { id: "showAll", value: 0, name: "Todos" },
+  { id: "5", value: 5, name: "5" },
+  { id: "10", value: 10, name: "10" },
+  { id: "15", value: 15, name: "15" },
+  { id: "20", value: 20, name: "20" },  
+];
